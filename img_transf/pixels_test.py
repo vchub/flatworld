4d242fe54c7dd5fa99936db232385285a8428ded
vlad chub <vlad.chub@gmail.com>
2020-12-19 22:27:04 -0800

    continue init

---
+++ img_transf/pixels_test.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+
+def rm_red(path) -> Image:
+    """Remove red from every pixel"""
+    img = Image.open(path)
+    h, w = img.size
+    # nimg = Image.new("RGB", (h, w), "black")  # create a new black image
+    ps = img.load()
+    for i in range(h):
+        for j in range(w):
+            r, g, b = ps[i, j]
+            ps[i, j] = (0, g, b)
+
+    return img
+
+
+def xtest_rm():
+    path = "./images/LutherBellPic.jpg"
+    img = rm_red(path)
+    img.show()
+
+
+if __name__ == "__main__":
+    path = "./images/LutherBellPic.jpg"
+    img = rm_red(path)
+    img.show()
---
+++ readme.md
@@ -0,0 +1,11 @@
+### Install required modules
+
+
+On windows run in terminal
+
+				py -m pip install -r requirements.txt
+
+Manually:
+
+				python3 -m pip install --upgrade pip
+				python3 -m pip install --upgrade Pillow
